34730b020d2443922b4082a4939da90cb8d1db5f
David McClure <dmcclure@pivotal.io>
2017-08-31 05:30:34 -0700

    Let's stop bowling on interrupt

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	fmt.Println("let's start bowling!")
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT)
+	<-signals
+	fmt.Println("exiting...")
 }
--- test/acceptance/acceptance_suite_test.go
+++ test/acceptance/acceptance_suite_test.go
@@ -1,7 +1,6 @@
 package acceptance_test
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 
@@ -15,6 +14,7 @@ import (
 
 var (
 	binPath string
+	session *gexec.Session
 )
 
 const TIMEOUT = "5s"
@@ -28,17 +28,24 @@ var _ = BeforeSuite(func() {
 	var err error
 	srcPath := os.Getenv("GOPATH") + "/src/github.com/mcwumbly/bowl-kata-pp-01/main.go"
 	binPath, err = gexec.Build(srcPath, "-race")
-	fmt.Printf("wrote binary to %s\n", binPath)
 	Expect(err).NotTo(HaveOccurred())
 })
 
 var _ = Describe("bowling kata++", func() {
-	It("prints to stdout when it starts up", func() {
+	BeforeEach(func() {
+		var err error
 		cmd := exec.Command(binPath)
-		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
+	})
 
+	AfterEach(func() {
+		session.Interrupt()
 		Eventually(session, TIMEOUT).Should(gexec.Exit(0))
-		Expect(session.Out).To(gbytes.Say("let's start bowling!"))
+		Eventually(session.Out).Should(gbytes.Say("exiting..."))
+	})
+
+	It("prints to stdout when it starts up", func() {
+		Eventually(session.Out).Should(gbytes.Say("let's start bowling!"))
 	})
 })
